0354c5628ee7ef6cc6464057f12f2bac26e72356
Arvid Fahlstrom Myrman <arvid.myrman@foundry.com>
2022-06-28 19:43:45 +0100

    add grpc tests

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-flix-dns-util: main.go
-	GODEBUG=netdns=cgo CGO_ENABLED=1 go build -o $@ $^
+flix-dns-util:main.go proto/flix_transfer.pb.go
+	GODEBUG=netdns=cgo CGO_ENABLED=1 go build -o $@ $<
+
+proto/flix_transfer.pb.go:proto/flix_transfer.proto
+	protoc -I proto --go_out=plugins=grpc:proto proto/flix_transfer.proto
--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
+
+	pb "github.com/TheFoundryVisionmongers/flix-dns-util/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 const (
@@ -36,7 +46,7 @@ func ctx() (context.Context, context.CancelFunc) {
 func log(line string, args ...interface{}) {
 	s := line
 	if len(args) > 0 {
-		s = fmt.Sprintf(line, args)
+		s = fmt.Sprintf(line, args...)
 	}
 	fmt.Printf(logFmt, timeStr(), s)
 }
@@ -49,8 +59,10 @@ func main() {
 	var srvAddr string
 	var srvPort int
 	var useTLS bool
+	var grpcPort int
 	flag.StringVar(&srvAddr, "hostname", "", "Hostname of the Flix server")
 	flag.IntVar(&srvPort, "port", 0, "Port of the Flix server")
+	flag.IntVar(&grpcPort, "transfer-port", 0, "File transfer port of the Flix server")
 	flag.BoolVar(&useTLS, "use-tls", false, "Use TLS when connecting to the Flix server")
 	flag.Parse()
 
@@ -60,6 +72,10 @@ func main() {
 	}
 	if srvPort == 0 {
 		log("Port is required. This should be the port of the Flix server, not including ':'")
+		os.Exit(1)
+	}
+	if grpcPort == 0 {
+		log("Transfer port is required. This should be the port used to transfer files to the Flix server, not including ':'")
 		os.Exit(1)
 	}
 	log("Address to lookup: %s", srvAddr)
@@ -157,20 +173,92 @@ func main() {
 	if useTLS {
 		proto = "https://"
 	}
-	url := fmt.Sprintf("%s%s:%d/info", proto, srvAddr, srvPort)
-	log("URL: %s", url)
-	resp, err := theClient.Get(url)
+	infoUrl := fmt.Sprintf("%s%s:%d/info", proto, srvAddr, srvPort)
+	log("URL: %s", infoUrl)
+	resp, err := theClient.Get(infoUrl)
 	if err != nil {
 		log("Failed to get info page: %v", err)
-		os.Exit(1)
+	} else {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log("Failed to read response: %v", err)
+		} else {
+			_ = resp.Body.Close()
+			log("Response body:")
+			log(string(body[:]))
+		}
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	grpcAddress := net.JoinHostPort(srvAddr, strconv.Itoa(grpcPort))
+
+	log("Attempting to get proxy information")
+	proxyUrl, err := http.ProxyFromEnvironment(&http.Request{
+		URL: &url.URL{
+			Scheme: "https",
+			Host:   grpcAddress,
+		},
+	})
 	if err != nil {
-		log("Failed to read response: %v", err)
-		os.Exit(1)
+		log("Could not get proxy information: %v", err)
+	} else if proxyUrl != nil {
+		log("Got proxy URL: %s", proxyUrl.String())
+	} else {
+		log("No proxy discovered")
 	}
-	_ = resp.Body.Close()
-	log("Response body:")
-	log(string(body[:]))
+
+	testGrpc("with default options", grpcAddress)
+	testGrpc("with no proxy", grpcAddress, grpc.WithNoProxy())
+}
+
+func testGrpc(name string, grpcAddress string, options ...grpc.DialOption) {
+	log("Connecting to %s over gRPC %s", grpcAddress, name)
+
+	grpcCreds := credentials.NewTLS(&tls.Config{
+		InsecureSkipVerify: true,
+		MinVersion:         tls.VersionTLS13,
+	})
+	interceptor := func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		log("Calling gRPC method %s on %s", method, cc.Target())
+		log("State: %v", cc.GetState())
+		return streamer(ctx, desc, cc, method, opts...)
+	}
+
+	c, cancel := ctx()
+	options = append(options, grpc.WithReturnConnectionError(), grpc.WithTransportCredentials(grpcCreds), grpc.WithStreamInterceptor(interceptor))
+	conn, err := grpc.DialContext(c, grpcAddress, options...)
+	cancel()
+	if err != nil {
+		log("Failed to dial server: %v", err)
+	} else {
+		client := pb.NewFileTransferClient(conn)
+
+		var header metadata.MD
+		var trailer metadata.MD
+		var peerInfo peer.Peer
+		log("Initiating transfer request")
+		c, cancel = ctx()
+		transferStream, err := client.Transfer(c, grpc.Header(&header), grpc.Trailer(&trailer), grpc.Peer(&peerInfo))
+		if err != nil {
+			log("Failed to call Transfer(): %v", err)
+		} else {
+			log("Sending transfer request")
+			if err = transferStream.Send(&emptypb.Empty{}); err != nil {
+				log("Failed to send transfer request: %v", err)
+			} else {
+				log("Receiving transfer response")
+				_, err = transferStream.Recv()
+				if err != nil && !strings.Contains(err.Error(), "FNAUTH signature not set") {
+					log("Failed to receive transfer response: %v", err)
+				}
+				log("Got transfer response")
+			}
+		}
+		cancel()
+
+		log("gRPC header: %v", header)
+		log("gRPC trailer: %v", trailer)
+		if peerInfo.Addr != nil {
+			log("gRPC peer information: %s %s", peerInfo.Addr.String(), peerInfo.Addr.Network())
+		}
+	}
 }
